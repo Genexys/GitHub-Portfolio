10a2727b8ad847794a730c5a333ad04e052a1fa5
Ilya Borisenko <magicilusha@gmail.com>
2023-07-07 17:29:07 +0300

    refactor: add prettier, sort repos

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,93 +1,100 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Link from "../components/Link/Link";
-import List from "../components/List/List";
+import Link from '../components/Link/Link';
+import List from '../components/List/List';
 
 const ProfileContainer = styled.div`
-    width: 50%;
-    margin: 20px auto;
+  width: 50%;
+  margin: 20px auto;
 `;
 
 const ProfileAvatar = styled.img`
-    display: block;
-    width: 150px;
-    margin: 0 auto 20px;
+  display: block;
+  width: 150px;
+  margin: 0 auto 20px;
 `;
 
 class Profile extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            data: {},
-            repos: [],
-            loading: true,
-            error: ''
-        }
+    this.state = {
+      data: {},
+      repos: [],
+      loading: true,
+      error: '',
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const profile = await fetch(`https://api.github.com/users/Genexys`);
+      const profileJSON = await profile.json();
+
+      if (profileJSON) {
+        const repos = await fetch(profileJSON.repos_url + '?per_page=50');
+        const reposJSON = await repos.json();
+
+        this.setState({
+          data: profileJSON,
+          repos: reposJSON,
+          loading: false,
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+
+      this.setState({
+        loading: false,
+        error: 'что то поломалось..((',
+      });
     }
+  }
 
-    async componentDidMount() {
-        try {
-            const profile = await fetch(`https://api.github.com/users/Genexys`);
-            const profileJSON = await profile.json();
+  render() {
+    const { data, repos, loading, error } = this.state;
 
-            if (profileJSON) {
-                const repos = await fetch(profileJSON.repos_url);
-                const reposJSON = await repos.json();
+    const arrData = [
+      { label: 'html_url', value: <Link url={data.html_url} title="GitHub url" /> },
+      { label: 'repos_url', value: data.repos_url },
+      { label: 'name', value: data.name },
+      { label: 'company', value: data.company },
+      { label: 'location', value: data.location },
+      { label: 'email', value: data.email },
+      { label: 'bio', value: data.bio },
+    ];
 
-                this.setState({
-                    data: profileJSON,
-                    repos: reposJSON,
-                    loading: false,
-                })
-            }
-        } catch (error) {
-            console.error(error.message)
+    const sortRepos = repos.sort((a, b) => {
+      const one = new Date(a?.updated_at).getTime() / 1000;
+      const two = new Date(b?.updated_at).getTime() / 1000;
 
-            this.setState({
-                loading: false,
-                error: 'что то поломалось..(('
-            })
-        }
-    }
+      return one < two ? 1 : -1;
+    });
 
-    render() {
-        const { data, repos, loading, error } = this.state;
+    const reposList = sortRepos.map((repo) => ({
+      label: repo.name,
+      value: <Link url={repo.html_url} title="link" />,
+    }));
 
-        const arrData = [
-            {label: 'html_url', value: <Link url={data.html_url} title="GitHub url" />},
-            {label: 'repos_url', value: data.repos_url},
-            {label: 'name', value: data.name},
-            {label: 'company', value: data.company},
-            {label: 'location', value: data.location},
-            {label: 'email', value: data.email},
-            {label: 'bio', value: data.bio}
-        ]
+    if (loading) return <div>Loading...</div>;
 
+    if (loading || error) return loading ? <div>Loading...</div> : error;
 
-        const reposList = repos.map(repo => ({
-            label: repo.name,
-            value: <Link url={repo.html_url} title="link" />
-        }))
-        if (loading) return <div>Loading...</div>;
+    return (
+      <ProfileContainer>
+        <ProfileAvatar src={data.avatar_url} alt="avatar" />
 
-        if (loading || error) return (loading ? <div>Loading...</div> : error);
+        <List titleList="Profile" arr={arrData} />
 
-        return (
-            <ProfileContainer>
-                <ProfileAvatar src={data.avatar_url} alt="avatar"/>
-
-                <List titleList="Profile" arr={arrData} />
-
-                <List titleList="Projects" arr={reposList} />
-            </ProfileContainer>
-        );
-    }
+        <List titleList="Projects" arr={reposList} />
+      </ProfileContainer>
+    );
+  }
 }
 
 Profile.propTypes = {
-    data: PropTypes.object,
+  data: PropTypes.object,
 };
 
 export default Profile;
